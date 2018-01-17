5e38315035b6c50f048256a72b3937042ab0efc5
alenija <alenija23@gmail.com>
2018-01-17 15:41:00 +0200

    tamplate

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,84 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+
+        </div>
+
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+            </ul>
+
+            <a class="navbar-brand" href="{{ url('/posts') }}">
+                Posts
+            </a>
+
+            @if (auth()->check())
+                @if (auth()->user()->isAdmin())
+                    <a class="navbar-brand" href="{{ url('/posts/create') }}">
+                        Create post
+                    </a>
+                @endif
+            @endif
+
+            <a class="navbar-brand" href="{{ url('about') }}">
+                About
+            </a>
+
+            <div class="dropdown">
+                <a class="navbar-brand dropdown-toggle"  data-toggle="dropdown" href="">
+                    Category
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">JavaScript</a></li>
+                </ul>
+            </div>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,27 +1,40 @@
 @extends('layouts.app')
+{{--@extends('layout')--}}
 
 @section('title', 'Page Posts')
 
 @section('content')
 
-    @if(Session::has('flash_message'))
-        <div class="alert alert-success">
-            {{ Session::get('flash_message') }}
+    <div class="album text-muted">
+        <div class="container">
+
+            <div class="row">
+
+                @if(Session::has('flash_message'))
+                <div class="alert alert-success">
+                {{ Session::get('flash_message') }}
+                </div>
+                @endif
+
+                <p><h1>All posts</h1></p>
+                @foreach ($posts as $post)
+                <p><h2>Id: {{ $post['id'] }}</h2></p>
+
+                 {{--for many to many--}}
+                    {{--Category:--}}
+                    {{--@foreach ($post['category'] as $category)--}}
+                    {{--<p>{{ $category['name'] }}</p>--}}
+                    {{--@endforeach--}}
+
+                <p>Category: {{ $post->category['name'] }}</p>
+                <p>Title: <a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post['title'] }}</a></p>
+                <p>Text: {{ $post['text'] }}</p>
+                <p>Author: {{ $post->user['name'] }}</p>
+                @endforeach
+
+            </div>
+
         </div>
-    @endif
+    </div>
 
-    <p><h1>All posts</h1></p>
-    @foreach ($posts as $post)
-        <p><h2>Id: {{ $post['id'] }}</h2></p>
-
-        {{-- for many to many --}}
-        {{--@foreach ($post['category'] as $category)--}}
-            {{--<p>Category: {{ $category['name'] }}</p>--}}
-        {{--@endforeach--}}
-
-        <p>Category: {{ $post->category['name'] }}</p>
-        <p>Title: <a href="{{ route('posts.show', ['post' => $post]) }}">{{ $post['title'] }}</a></p>
-        <p>Text: {{ $post['text'] }}</p>
-        <p>Author: {{ $post->user['name'] }}</p>
-    @endforeach
 @endsection
